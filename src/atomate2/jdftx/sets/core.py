7b5921f1c27bdfb7b5460d308e146dfe5e97ab94
Sophie Gerits <soge8904@colorado.edu>
2025-03-31 21:17:00 -0600

    changes to SCANSetGenerator

--- src/atomate2/jdftx/sets/core.py
+++ src/atomate2/jdftx/sets/core.py
@@ -74,6 +74,8 @@ class SCANSetGenerator(JdftxInputGenerator):
             "fluid-cation": {"name": "Na+", "concentration": 0.5},
             "fluid-anion": {"name": "F-", "concentration": 0.5},
             "elec-ex-corr": {"funcX": "mgga-x-r2scan","funcC": "mgga-c-r2scan"},
-            "ionic-minimize": {"nIterations": 100}
+            "ionic-minimize": {"nIterations": 100},
+            "ion-species": "/projects/beast/Software/JDFTx/build-gnu/pseudopotentials/SG15/$ID_ONCV_PBE.upf",
+            "van-der-waals": None
         }
     )
